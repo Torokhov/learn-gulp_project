32205fa8329ed7e76167b5d84bb89e573727b4c1
Oleg Torokhov <torokhov093@gmail.com>
2017-03-25 15:54:11 +0700

    add tasks styles, clean, assets, build

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict'
+const gulp = require('gulp');
+const less = require('gulp-less');
+//const concat = require('gulp-concat');
+const debug = require('gulp-debug');
+const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+const del = require('del');
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+gulp.task('styles', function() {
+  return gulp.src('frontend/styles/main.less')
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(less())
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+    .pipe(gulp.dest('public'));
+  
+});
+
+gulp.task('clean', function() {
+  return del('public');
+});
+
+gulp.task('assets', function() {
+  return gulp.src('frontend/assets/**')
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'assets')));
